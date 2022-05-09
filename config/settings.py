e0e5d3000ccc2b899f9b768575655815a12bcd04
ayoeze191 <olabodeezekiel2018@yahoo.com>
2022-05-09 08:48:18 +0100

    asa

--- config/settings.py
+++ config/settings.py
@@ -93,17 +93,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
-# hello
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE' : 'django.db.backends.postgresql',
