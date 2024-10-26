cc208e1079f77ad6c6f68577dce8b4fe7bc88d2a
ayoeze191 <ayoeze191@gmail.com>
2024-10-26 11:59:36 -0400

    redeploy

--- config/settings.py
+++ config/settings.py
@@ -116,9 +116,9 @@ DATABASES = {
     }
 }
 if os.getenv("DJANGO_ENV") == 'production':
-    DATABASES = {
-        'default': dj_database_url.config(default = 'postgres://localhost') 
-    }
+    # DATABASES = {
+    #     'default': dj_database_url.config(default = 'postgres://localhost') 
+    # }
     # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     STATIC_ROOT = 'staticfiles'
